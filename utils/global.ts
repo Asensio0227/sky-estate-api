ee60338aaac9870bbabcd42d0ee20333d9c649b2
Asensio0227 <mtheesikhosana@gmail.com>
2025-04-12 10:54:49 +0200

    limit commit

--- utils/global.ts
+++ utils/global.ts
@@ -29,7 +29,7 @@ export function queryFilters(req: Request, sort?: string, sortKeys?: string) {
   const sortParam = isValidSortKey(sort) ? sort : 'string';
   const sortKey = sortOptions[sortParam] || sortOptions.newest;
   const page = Number(req.query.page) || 1;
-  const limit = Number(req.query.limit) || 10;
+  const limit = Number(req.query.limit) || 20;
   const skip = (page - 1) * limit;
 
   return {
